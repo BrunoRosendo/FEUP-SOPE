b9dc8b446c072e0a2a092efdcce12e35a69f3d40
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-03-03 15:24:20 +0000

    Parsing functions

--- Proj1/src/main.c
+++ Proj1/src/main.c
@@ -3,22 +3,81 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <stdbool.h>
+#include <ctype.h>
 
-#define SIMPLE 0
-#define VERBOSE 1
-#define ONCHANGE 2
+typedef enum outputOption {simple, verbose, onChange} outputOption;
+typedef enum userType {owner, group, others, all} userType;
+typedef enum permAction {erase, add, substitute} permAction;
 
-struct Options {
-    bool octal; // octal or string?
+typedef struct Options {
+    bool octal;  // octal or string?
     bool recursive;
-    int output; // simple, verbose or on change?
+    outputOption output;
+    userType user;
+    permAction action;
 } Options;
 
+// assumes valid arguments
 void changePermsWithOctal(const char *pathname, mode_t mode) {
     chmod(pathname, mode);
 }
 
-void parseMode(const char * modeString) {
+// assumes valid arguments
+void changePermsWithString(const char *pathname, const char *modeString, Options *options) {
+    mode_t mode = 0;
+    switch (options->user) {
+        case owner:
+            if (modeString[0] == 'r')
+                mode |= 0400;
+            if (modeString[1] == 'w')
+                mode |= 0200;
+            if (modeString[2] == 'x')
+                mode |= 0100;
+            break;
+        case group:
+            if (modeString[0] == 'r')
+                mode |= 0040;
+            if (modeString[1] == 'w')
+                mode |= 0020;
+            if (modeString[2] == 'x')
+                mode |= 0010;
+            break;
+        case others:
+            if (modeString[0] == 'r')
+                mode |= 0004;
+            if (modeString[1] == 'w')
+                mode |= 0002;
+            if (modeString[2] == 'x')
+                mode |= 0001;
+            break;
+        case all:
+            if (modeString[0] == 'r')
+                mode |= 0400;
+            if (modeString[1] == 'w')
+                mode |= 0200;
+            if (modeString[2] == 'x')
+                mode |= 0100;
+            if (modeString[3] == 'r')
+                mode |= 0040;
+            if (modeString[4] == 'w')
+                mode |= 0020;
+            if (modeString[5] == 'x')
+                mode |= 0010;
+            if (modeString[6] == 'r')
+                mode |= 0004;
+            if (modeString[7] == 'w')
+                mode |= 0002;
+            if (modeString[8] == 'x')
+                mode |= 0001;
+            break;
+        default:
+            fprintf(stderr, "Invalid userType\n");
+            exit(3);
+    }
+    changePermsWithOctal(pathname, mode);
+}
+
+void parseMode(const char *modeString, Options *options, char* cutString) {
     // Check if it's in octal mode
     if (strlen(modeString) == 4) {
         bool valid = true;
@@ -32,18 +91,66 @@ void parseMode(const char * modeString) {
             printf("Use the format 0XXX where X is between 0 and 7\n");
             exit(2);
         }
+        options->octal = true;
+        options->action = substitute;
+        options->user = all;
+        return;
     }
+
+    // Start parsing the string mode
+
+    int i = 0;
+    options->user = all;  // default
+    if (isalpha(modeString[0])) {  // userType specified
+        ++i;
+        switch (modeString[0]) {
+            case 'u':
+                options->user = owner;
+                break;
+            case 'g':
+                options->user = group;
+                break;
+            case 'o':
+                options->user = others;
+                break;
+            case 'a':
+                break;
+            default:
+                fprintf(stderr, "Invalid input (userType)\n");
+                exit(3);
+        }
+    }
+    switch (modeString[i]) {
+        case '-':
+            options->action = erase;
+            break;
+        case '+':
+            options->action = add;
+            break;
+        case '=':
+            options->action = substitute;
+            break;
+        default:
+            fprintf(stderr, "Invalid input (permission action)\n");
+            exit(3);
+    }
+    i++; int j; // WATCH OUT
+    for (j = 0; modeString[i] != '\0'; ++i, ++j)
+        cutString[j] = modeString[i];
+    ++j;
+    cutString[j] = '\0';
 }
 
 int main(int argc, char* argv[]) {
-
     if (argc < 3) {
         fprintf(stderr, "Wrong number of arguments! ");
         printf("Call the function with:\nxmod [OPTIONS] MODE FILE/DIR\n");
         exit(1);
     }
 
-    parseMode(argv[1]);
+    Options options;
+    char modeString[10];
+    parseMode(argv[argc - 2], &options, &modeString);
 
     mode_t mode = (mode_t) atoi(argv[1]);
     changePermsWithOctal(argv[2], mode);
