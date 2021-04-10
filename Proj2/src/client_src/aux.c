33f75af1a7a0410c761909e9f05707b295c34924
monkin77 <monkinsmurf@gmail.com>
2021-04-10 21:58:30 +0100

    Implemented the parseCMDArgs function

--- Proj2/src/client_src/aux.c
+++ Proj2/src/client_src/aux.c
@@ -1,7 +1,27 @@
 #include "aux.h"
 
-void parseCMDArgs(char* argv[], Settings* settings) {
+int parseCMDArgs(char* argv[], Settings* settings) {
+    char* timeFlag = argv[1];
+    
+    if(strcmp(timeFlag, "-t")){     // returns non-zero if not equal
+        fprintf(stderr, "Invalid 2nd argument! Should be '-t'\n");
+        return 1;
+    }
 
+    int runningTime = atoi(argv[2]);
+    if(runningTime < 1){
+        fprintf(stderr, "Invalid 3rd argument! The specified time should be an integer greater than 1\n");
+        return 1;
+    }
+
+    char* fifoName = argv[3];
+    if(!strcmp(fifoName, "")){
+        fprintf(stderr, "Invalid 4th argument! You need to specifiy a name for the public FIFO\n");
+        return 1;
+    }
+
+    settings->execTime = runningTime, settings->fifoname = fifoName;
+    return 0;
 }
 
 void syncWithServer(Settings* settings) {
@@ -13,6 +33,8 @@ void syncWithServer(Settings* settings) {
 }
 
 void init(char* argv[], Settings* settings) {
-    parseCMDArgs(argv, settings);
+    if(parseCMDArgs(argv, settings))
+        return;
+
     syncWithServer(settings);
 }
--- Proj2/src/client_src/aux.h
+++ Proj2/src/client_src/aux.h
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
 
 #define FIFO_PUBLIC_PERMS 0666
 
@@ -19,11 +20,12 @@ Makes all the needed operations for the start of the program
 */
 void init(char* argv[], Settings* settings);
 
-/*
-Parses the command line arguments and puts the information in the
-Settings struct
-*/
-void parseCMDArgs(char* argv[], Settings* settings);
+/**
+ * Parses the command line arguments and puts the information in
+ * the Settings struct
+ * @return 0 on success, 1 otherwise
+ */ 
+int parseCMDArgs(char* argv[], Settings* settings);
 
 /*
 Creates a FIFO (if it doesn't exist) and waits for the server to synchronize
